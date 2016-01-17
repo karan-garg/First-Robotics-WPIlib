29f4fd37bd7d5a7c286a18e62e7d5102aceb6d62
karan-garg <karangarg01001@gmail.com>
2016-01-17 00:38:19 -0500

    Local files published

---
+++ Getting Started/src/org/usfirst/frc/team5631/robot/Robot.java
@@ -0,0 +1,94 @@
+package org.usfirst.frc.team5631.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive myRobot;
+	Joystick stick;
+	int autoLoopCounter;
+	
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	myRobot = new RobotDrive(0,1);
+    	stick = new Joystick(0);
+    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
+    public void autonomousInit() {
+    	autoLoopCounter = 0;
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobot.drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobot.drive(0.0, 0.0); 	// stop robot
+		}
+    }
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
+    public void teleopInit(){
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        myRobot.arcadeDrive(stick);
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run();
+    }
+    
+    public void goToGoal(){
+    	//get initial orientation, initial position of robot
+    	double initAngle = 0;
+    	double initPos[] = new double[2];
+    	double turnAngle;
+    	double distToGoal;
+    	
+    	//set goal
+    	double goalPos[] = new double[2];
+    	
+    	//compute turn angle
+    	distToGoal = (goalPos[0] - initPos[0])/(goalPos[1] - initPos[1]);
+    	turnAngle = Math.atan2(Math.sin(distToGoal), Math.cos(distToGoal)) - initAngle;
+    	
+    	//PID
+    	PIDController angleController = new PIDController(1.1,.1,0, null, null);
+    	angleController.setSetpoint(turnAngle);
+    	angleController.
+    	
+    	
+    }
+    
+}
